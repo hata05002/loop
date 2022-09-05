3a26b9203610eee6c47365b2b9dacf511ebaaa8c
hata05002 <112382754+hata05002@users.noreply.github.com>
2022-09-05 09:39:16 +0200

    Add files via upload

---
+++ Loop program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace loop{
+
+    class program {
+        static void Main(){
+             int tal= 1;
+             while (tal<=20){
+            Console.WriteLine(tal+1);
+        }
+        }
+}
+
+ }
