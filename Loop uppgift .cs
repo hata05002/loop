aabbc1eeac551af06928e92eb32bd4ce30d8e401
hata05002 <112382754+hata05002@users.noreply.github.com>
2022-09-05 09:41:17 +0200

    Add files via upload

---
+++ Loop uppgift .cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace tal {
+    class program{
+     static void Main(){
+            int tal= 8;
+            while (tal>=1 && tal<= 100){
+                Console.Write(tal+1);
+                if (tal==101)
+                    break;
+                
+            }
+            }
+} 
+
+}
