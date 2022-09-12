bc3d28d2c7fb271318bbdbfa9cf67e2820967163
hata05002 <112382754+hata05002@users.noreply.github.com>
2022-09-12 10:29:57 +0200

    Add files via upload

---
+++ loop reWrite.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace looprewrite{
+    class program{
+        static void Main() {
+            int tal;
+            for (tal=1; tal<=20; tal++){
+                Console.WriteLine(tal);
+            }
+        }
+    }
+}
